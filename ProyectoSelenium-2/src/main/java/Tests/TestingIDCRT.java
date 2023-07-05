7ef559ac4fba304cfd9b124218ad2d42ce6c1bfa
Agustin Gonzalo Togni Balassi <122832468+AgustinTogni@users.noreply.github.com>
2023-07-05 16:12:00 -0300

    Add files via upload

--- ProyectoSelenium-2/src/main/java/Tests/TestingIDCRT.java
+++ ProyectoSelenium-2/src/main/java/Tests/TestingIDCRT.java
@@ -115,6 +115,94 @@ public class TestingIDCRT {
         }
     }
 
+    @Test (priority = 2, enabled = true)
+    public void AddTakeOutProductToTheCartCRT002() throws InterruptedException {}
+
+    @Test (priority = 3, enabled = true)
+    public void DeleteProductToTheCartWithLightBlueTrashCRT003() throws InterruptedException {
+
+        // ----- Test for Chrome -----
+
+        // The "PC Basic" section is clicked.
+        chromeDriver.findElement(By.xpath("//*[@id=\"home\"]/section[2]/div[2]/div/div/div[1]/div/div/div/a/picture/img")).click();
+
+        // Wait for the section to charge.
+        By seeMoreBtnSelectorCM = By.xpath("//*[@id=\"9555\"]/div/div[3]/div[4]/a");
+        WebElement seeMoreBtnCM = chromeWait.until(ExpectedConditions.visibilityOfElementLocated(seeMoreBtnSelectorCM));
+
+        // The "Ver mas" button is clicked.
+        seeMoreBtnCM.click();
+
+        // Wait for the "AGREGAR AL CARRITO" button to appear.
+        By addToCartSelectorCM = By.xpath("//*[@id=\"contenedorproducto\"]/div[2]/input");
+        WebElement addToCartCM = chromeWait.until(ExpectedConditions.visibilityOfElementLocated(addToCartSelectorCM));
+
+        // The "AGREGAR AL CARRITO" button is clicked.
+        addToCartCM.click();
+
+        // Wait for the "No" button to appear.
+        By optionNoSelectorCM = By.xpath("//*[@id=\"modalseguircomprando\"]/div/div/div[2]/div/div/button[2]");
+        WebElement optionNoCM = chromeWait.until(ExpectedConditions.visibilityOfElementLocated(optionNoSelectorCM));
+
+        // The "No" button is clicked.
+        optionNoCM.click();
+
+        // The "Light Blue Trash" icon is clicked.
+        chromeDriver.findElement(By.xpath("//*[@id=\"eliminar\"]/img")).click();
+
+        // Wait for the alert to appear.
+        By removedProductAlertSelectorCM = By.xpath("//*[@id=\"modalError\"]/div/div");
+        WebElement removedProductAlertCM = chromeWait.until(ExpectedConditions.visibilityOfElementLocated(removedProductAlertSelectorCM));
+
+        // It is verified that the product was removed the cart.
+        if (removedProductAlertCM.isDisplayed()) {
+            System.out.println("The product was removed the cart in Chrome.");
+        } else {
+            Assert.fail("The product was not removed the cart in Chrome.");
+        }
+
+        // ----- Test for Microsoft Edge -----
+
+        // The "PC Basic" section is clicked.
+        edDriver.findElement(By.xpath("//*[@id=\"home\"]/section[2]/div[2]/div/div/div[1]/div/div/div/a/picture/img")).click();
+
+        // Wait for the section to charge.
+        By seeMoreBtnSelectorME = By.xpath("//*[@id=\"9555\"]/div/div[3]/div[4]/a");
+        WebElement seeMoreBtnME = edWait.until(ExpectedConditions.visibilityOfElementLocated(seeMoreBtnSelectorME));
+
+        // The "Ver mas" button is clicked.
+        seeMoreBtnME.click();
+
+        // Wait for the "AGREGAR AL CARRITO" button to appear.
+        By addToCartSelectorME = By.xpath("//*[@id=\"contenedorproducto\"]/div[2]/input");
+        WebElement addToCartME = edWait.until(ExpectedConditions.visibilityOfElementLocated(addToCartSelectorME));
+
+        // The "AGREGAR AL CARRITO" button is clicked.
+        addToCartME.click();
+
+        // Wait for the "No" button to appear.
+        By optionNoSelectorME = By.xpath("//*[@id=\"modalseguircomprando\"]/div/div/div[2]/div/div/button[2]");
+        WebElement optionNoME = edWait.until(ExpectedConditions.visibilityOfElementLocated(optionNoSelectorME));
+
+        // The "No" button is clicked.
+        optionNoME.click();
+
+        // The "Light Blue Trash" icon is clicked.
+        edDriver.findElement(By.xpath("//*[@id=\"eliminar\"]/img")).click();
+
+        // Wait for the alert to appear.
+        By removedProductAlertSelectorME = By.xpath("//*[@id=\"modalError\"]/div/div");
+        WebElement removedProductAlertME = edWait.until(ExpectedConditions.visibilityOfElementLocated(removedProductAlertSelectorME));
+
+        // It is verified that the product was removed the cart.
+        if (removedProductAlertME.isDisplayed()) {
+            System.out.println("The product was removed the cart in Microsoft Edge.");
+        } else {
+            Assert.fail("The product was not removed the cart in Microsoft Edge.");
+        }
+
+    }
+
     @AfterMethod
     public void CloseSession(){
         log.info("#######");
