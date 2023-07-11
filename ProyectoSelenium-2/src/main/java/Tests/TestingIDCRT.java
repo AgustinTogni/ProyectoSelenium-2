5b68168573859670fc2dab48909b0a18ddedc18d
Agustin Gonzalo Togni Balassi <122832468+AgustinTogni@users.noreply.github.com>
2023-07-11 16:20:28 -0300

    Add files via upload

--- ProyectoSelenium-2/src/main/java/Tests/TestingIDCRT.java
+++ ProyectoSelenium-2/src/main/java/Tests/TestingIDCRT.java
@@ -288,6 +288,98 @@ public class TestingIDCRT {
 
     }
 
+    @Test (priority = 5, enabled = true)
+    public void ContinueToPaymentCRT005() throws InterruptedException {
+
+        // ----- Test for Chrome -----
+
+        // The "PC Basic" section is clicked.
+        chromeDriver.findElement(By.xpath("//*[@id=\"home\"]/section[2]/div[2]/div/div/div[1]/div/div/div/a/picture/img")).click();
+
+        // Wait for the section to charge.
+        By seeMoreBtnSelectorCM = By.xpath("//*[@id=\"9555\"]/div/div[3]/div[4]/a");
+        WebElement seeMoreBtnCM = chromeWait.until(ExpectedConditions.visibilityOfElementLocated(seeMoreBtnSelectorCM));
+
+        // The "Ver mas" button is clicked.
+        seeMoreBtnCM.click();
+
+        // Wait for the "AGREGAR AL CARRITO" button to appear.
+        By addToCartSelectorCM = By.xpath("//*[@id=\"contenedorproducto\"]/div[2]/input");
+        WebElement addToCartCM = chromeWait.until(ExpectedConditions.visibilityOfElementLocated(addToCartSelectorCM));
+
+        // The "AGREGAR AL CARRITO" button is clicked.
+        addToCartCM.click();
+
+        // Wait for the "No" button to appear.
+        By optionNoSelectorCM = By.xpath("//*[@id=\"modalseguircomprando\"]/div/div/div[2]/div/div/button[2]");
+        WebElement optionNoCM = chromeWait.until(ExpectedConditions.visibilityOfElementLocated(optionNoSelectorCM));
+
+        // The "No" button is clicked.
+        optionNoCM.click();
+
+        // Wait for the "CONTINUAR" button to appear.
+        By continueButtonSelectorCM = By.xpath("//*[@id=\"cart-buy-btn\"]");
+        WebElement continueButtonCM = chromeWait.until(ExpectedConditions.elementToBeClickable(continueButtonSelectorCM));
+
+        // The "CONTINUAR" button is clicked.
+        continueButtonCM.click();
+
+        // Wait fot the login window to appear.
+        By loginWindowSelectorCM = By.xpath("//*[@id=\"loginBody\"]/div");
+        WebElement loginWindowCM = chromeWait.until(ExpectedConditions.elementToBeClickable(loginWindowSelectorCM));
+
+        // It is verified that login window is visible.
+        if (loginWindowCM.isDisplayed()) {
+            System.out.println("The login window is visible in Chrome.");
+        } else {
+            Assert.fail("The login window is not visible in Chrome.");
+        }
+
+        // ----- Test for Microsoft Edge -----
+
+        // The "PC Basic" section is clicked.
+        edDriver.findElement(By.xpath("//*[@id=\"home\"]/section[2]/div[2]/div/div/div[1]/div/div/div/a/picture/img")).click();
+
+        // Wait for the section to charge.
+        By seeMoreBtnSelectorME = By.xpath("//*[@id=\"9555\"]/div/div[3]/div[4]/a");
+        WebElement seeMoreBtnME = edWait.until(ExpectedConditions.visibilityOfElementLocated(seeMoreBtnSelectorME));
+
+        // The "Ver mas" button is clicked.
+        seeMoreBtnME.click();
+
+        // Wait for the "AGREGAR AL CARRITO" button to appear.
+        By addToCartSelectorME = By.xpath("//*[@id=\"contenedorproducto\"]/div[2]/input");
+        WebElement addToCartME = edWait.until(ExpectedConditions.visibilityOfElementLocated(addToCartSelectorME));
+
+        // The "AGREGAR AL CARRITO" button is clicked.
+        addToCartME.click();
+
+        // Wait for the "No" button to appear.
+        By optionNoSelectorME = By.xpath("//*[@id=\"modalseguircomprando\"]/div/div/div[2]/div/div/button[2]");
+        WebElement optionNoME = edWait.until(ExpectedConditions.visibilityOfElementLocated(optionNoSelectorME));
+
+        // The "No" button is clicked.
+        optionNoME.click();
+
+        // Wait for the "CONTINUAR" button to appear.
+        By continueButtonSelectorME = By.xpath("//*[@id=\"cart-buy-btn\"]");
+        WebElement continueButtonME = edWait.until(ExpectedConditions.elementToBeClickable(continueButtonSelectorME));
+
+        // The "CONTINUAR" button is clicked.
+        continueButtonME.click();
+
+        // Wait fot the login window to appear.
+        By loginWindowSelectorME = By.xpath("//*[@id=\"loginBody\"]/div");
+        WebElement loginWindowME = edWait.until(ExpectedConditions.elementToBeClickable(loginWindowSelectorME));
+
+        // It is verified that login window is visible.
+        if (loginWindowME.isDisplayed()) {
+            System.out.println("The login window is visible in in Microsoft Edge.");
+        } else {
+            Assert.fail("The login window is not visible in in Microsoft Edge.");
+        }
+    }
+
     @AfterMethod
     public void CloseSession(){
         log.info("#######");
